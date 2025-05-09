61700606b17e5dc3bced05f9d944f77145df2448
C-500 <maxigiaco94@hotmail.com>
2025-05-09 14:03:35 -0300

    Add finished exercise 6.4

---
+++ Unidad6/Ejercicio6.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << "Escriba su nombre (SIN ESPACIOS): " << endl;
+    string nombre;
+    cin >> nombre;
+
+    cout << "Hola " << nombre << "!" << endl;
+    
+    return 0;
+}
