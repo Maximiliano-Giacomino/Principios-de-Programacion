2d9dc64e1adccb3c6c0566abec1a274061b0affc
C-500 <maxigiaco94@hotmail.com>
2025-05-08 23:57:51 -0300

    Organized initial files in "Unidad6" folder
    The starting scattered files are now stored within a folder, from now on
    each week's files should be organized in that way. I've also finished
    my own version of the example program the teacher showed in the
    theory class.

---
+++ Unidad6/Teoría_06.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Se desea construir un programa donde el usuario ingrese el largo de los lados de un triángulo
+//rectángulo. En base a estos datos se debe calcular el largo de la hipotenusa, el área y el 
+//perímetro del triángulo
+
+int main()
+{
+    cout << "Inserte los largos de los lados de su triéngulo: " << endl;
+    
+    int lado_a, lado_b;
+    cin >> lado_a;
+    cin >> lado_b;
+    //Bloque de ingreso de datos
+
+    //cout << "The inputs are: " << lado_a << " and " << lado_b;
+
+    float hipot = sqrt((lado_a*lado_a)+(lado_b*lado_b));
+    float perim = lado_a + lado_b + hipot;
+    float area = (lado_a * lado_b)/2;
+
+    cout << "Los resultados fueron: " << endl;
+    cout << "Hipotenusa: " << hipot;
+    cout << " Perimetro: " << perim;
+    cout << " Area: " << area;
+
+    return 0;
+}
